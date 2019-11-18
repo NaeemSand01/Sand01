69a239bc717ebaa76fc3c043ebb8745724e025a9
Naeem Sanders <53099123+NaeemSand01@users.noreply.github.com>
2019-11-18 08:45:48 -0800

    Adding Recipe App Footer Component
    Created a recipe app for CRUD APP practice, adding footer component and css style.

--- Footer.jsx
+++ Footer.jsx
@@ -1,121 +1,54 @@
-import React, { Component } from "react";
+import React from "react";
 import "./css/footer.css";
 
-class Footer extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="footer foot">
-          <div className="container footer-contain">
-            <div className="row">
-              <div className="col-md-2 col-12">
-                <div className="footerLink">
-                  <h5>ACCESSORIES</h5>
-                  <ul className="list-unstyled">
-                    <li>
-                      <a href="...">Link</a>
-                    </li>
-                    <li>
-                      <a href="...">Link2</a>
-                    </li>
-                    <li>
-                      <a href="...">Link3</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md-2 col-12">
-                <div className="footerLink">
-                  <h5>BRANDS</h5>
-                  <ul className="list-unstyled">
-                    <li>
-                      <a href="...">Link</a>
-                    </li>
-                    <li>
-                      <a href="...">Link2</a>
-                    </li>
-                    <li>
-                      <a href="...">Link3</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md-2 col-12">
-                <div className="footerLink">
-                  <h5>ACCESSORIES</h5>
-                  <ul className="list-unstyled">
-                    <li>
-                      <a href="...">Link</a>
-                    </li>
-                    <li>
-                      <a href="...">Link2</a>
-                    </li>
-                    <li>
-                      <a href="...">Link3</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md-2 col-12">
-                <div className="footerLink">
-                  <h5>GET IN TOUCH</h5>
-                  <ul className="list-unstyled">
-                    <li>Call us at (777)-777-7777</li>
-                    <li>
-                      <a href="...">naeem.sanders0@gmail.com</a>
-                    </li>
-                  </ul>
-                  <ul className="list-inline">
-                    <li>
-                      <a href="...">
-                        <i className="fab fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="...">
-                        <i className="fab fa-facebook" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="...">
-                        <i className="fab fa-github" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-md-4 col-12">
-                <div className="newsletter news">
-                  <h4>Newsletter</h4>
-                  <h3>Sign up now</h3>
-                  <p>
-                    Enter your email address and get notified about new
-                    products.
-                  </p>
-                  <div className="input-group">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="your email"
-                      aria-describedby="basic-addon2"
-                    />
-                    <a
-                      href="..."
-                      className="input-group-addon"
-                      id="basic-addon2"
-                    >
-                      go
-                      <i className="fa fa-chevron-right" />
+const Footer = props => {
+  console.log(props);
+  return (
+    <React.Fragment>
+      <footer className="bg-secondary da-section">
+        <div className="container contain-text">
+          <div className="row">
+            <div className="col-md-4 col-12">
+              <div className="h2">Lets Eat!</div>
+              <p>naeem.sanders0@gmail.com</p>
+            </div>
+
+            <div className="col-md-4 col-12">
+              <div className="footerLink">
+                <h5>Follow Us!</h5>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="..." className="da-social-link">
+                      Facebook <i className="fab fa-facebook" />
                     </a>
-                  </div>
-                </div>
+                  </li>
+                  <li>
+                    <a href="..." className="da-social-link">
+                      Twitter <i className="fab fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="..." className="da-social-link">
+                      Instagram <i className="fab fa-instagram" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="..." className="da-social-link">
+                      Google <i className="fab fa-google" />
+                    </a>
+                  </li>
+                </ul>
               </div>
+            </div>
+            <div className="col-md-4">
+              <div className="h6 pb-2">Copyright</div>
+              <p>@ 2019 Pandapp. All rights reserved</p>
             </div>
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
-}
+      </footer>
+    </React.Fragment>
+  );
+};
 
 export default Footer;
